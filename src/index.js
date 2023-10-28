33837e49265b8076afe158c342712b9f75346c4e
ViniCaon <vinicaonita@hotmail.com>
2023-10-28 11:59:38 -0300

    Bloco de notas

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+app.whenReady()
+    .then(function () {
+        const janela = new BrowserWindow({
+            height: 550,
+            width: 450,
+            frame: false,
+            resizable: false;
+        })
+
+        janela.loadFile("./public/index.html")
+
+        })
